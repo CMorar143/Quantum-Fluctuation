9464e81df89c90dc0f96890a848aa8cd35d70950
CMorar143 <cian@morar.org>
2019-11-21 23:35:25 +0000

    spring joint working between particles

---
+++ Quantum-Fluctuation/Assets/Scripts/ParticleBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleBehaviour : MonoBehaviour
+{
+    public GameObject Matter, AntiMatter;
+    private Transform startMattter, startAntiMatter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startMattter = Matter.transform;
+        startAntiMatter = AntiMatter.transform;
+        Invoke("MoveParticles", 3);
+    }
+
+    public void MoveParticles()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
