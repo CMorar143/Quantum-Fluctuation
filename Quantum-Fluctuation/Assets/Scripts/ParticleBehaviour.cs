30de10b5e5a08f0b51e84e56b7c0b854586dcee3
CMorar143 <cian@morar.org>
2019-11-22 02:15:48 +0000

    paticles attract each other

--- Quantum-Fluctuation/Assets/Scripts/ParticleBehaviour.cs
+++ Quantum-Fluctuation/Assets/Scripts/ParticleBehaviour.cs
@@ -5,23 +5,27 @@ using UnityEngine;
 public class ParticleBehaviour : MonoBehaviour
 {
     public GameObject Matter, AntiMatter;
-    private Transform startMattter, startAntiMatter;
+    private Transform startMarker;
+    private Transform endMarker;
+    private float speed = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        startMattter = Matter.transform;
-        startAntiMatter = AntiMatter.transform;
-        Invoke("MoveParticles", 3);
+        startMarker = endMarker = this.transform;
+        //endMarker.position += new Vector3(0, 4);
+        Invoke("MoveParticles", 1);
     }
 
     public void MoveParticles()
     {
-
+        this.GetComponent<Rigidbody>().AddForce(0, 0.5f, 0, ForceMode.Impulse);
+        Matter.GetComponent<Rigidbody>().AddForce(1, 0, 0, ForceMode.Impulse);
+        AntiMatter.GetComponent<Rigidbody>().AddForce(-1, 0, 0, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
