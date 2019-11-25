8fb08fb462f52927ddedb3d365a03fff493102a7
CMorar143 <cian@morar.org>
2019-11-25 02:01:14 +0000

    need to generate particle in front of the camera

---
+++ Quantum-Fluctuation/Assets/Scripts/ParticleGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleGenerator : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public GameObject particles;
+
+    // This script will simply instantiate the Prefab when the game starts.
+    void Start()
+    {
+        Instantiate(particles, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
