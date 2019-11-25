4be289e7bb26d713ae92f058f5e6411feda01a53
CMorar143 <cian@morar.org>
2019-11-25 01:12:32 +0000

    addedplayer controller

--- Quantum-Fluctuation/Assets/Scripts/PlayerController.cs
+++ Quantum-Fluctuation/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Controller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public float speed = 5f;
+    public float rotspeed = 65f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        transform.Rotate(0f, Input.GetAxis("Horizontal") * Time.deltaTime * rotspeed, 0f);
     }
 }
