d84ef1674519d23565ddb3f74b947f6a295e73f2
CMorar143 <cian@morar.org>
2019-11-25 01:25:32 +0000

    going to create an FPS controller instead

--- Quantum-Fluctuation/Assets/Scripts/PlayerController.cs
+++ Quantum-Fluctuation/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5f;
-    public float rotspeed = 65f;
+    public GameObject mainCamera;
+    public float speed = 50.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0f, 0f, Input.GetAxis("Vertical") * Time.deltaTime * speed);
-        transform.Rotate(0f, Input.GetAxis("Horizontal") * Time.deltaTime * rotspeed, 0f);
+        
     }
 }
