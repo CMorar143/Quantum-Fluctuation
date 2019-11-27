1704a5abde21f86906379d6c770903b361f55862
CMorar143 <cian@morar.org>
2019-11-27 02:13:49 +0000

    added random range

--- Quantum-Fluctuation/Assets/Scripts/ParticleGenerator.cs
+++ Quantum-Fluctuation/Assets/Scripts/ParticleGenerator.cs
@@ -11,6 +11,6 @@ public class ParticleGenerator : MonoBehaviour
     {
         //Instantiate(particles, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1.5f), Quaternion.identity);
         playerPos = transform.position;
-        Instantiate(particles, new Vector3(transform.position.x, 0, transform.position.z), transform.rotation);
+        Instantiate(particles, new Vector3(transform.position.x + Random.Range(-0.4f, 0.4f), Random.Range(-0.3f, 0.2f), transform.position.z), transform.rotation);
     }
 }
