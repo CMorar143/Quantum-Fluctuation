02f1e3b417af37c83d047b6711f342765085f0a5
CMorar143 <cian@morar.org>
2019-11-27 01:49:33 +0000

    trying to get the particles to spawn infront of the camera

--- Quantum-Fluctuation/Assets/Scripts/ParticleGenerator.cs
+++ Quantum-Fluctuation/Assets/Scripts/ParticleGenerator.cs
@@ -6,10 +6,14 @@ public class ParticleGenerator : MonoBehaviour
 {
     // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
     public GameObject particles;
+    
 
     // This script will simply instantiate the Prefab when the game starts.
     void Start()
     {
-        Instantiate(particles, new Vector3(0, 0, 0), Quaternion.identity);
+        GameObject p = Instantiate(particles, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        p.transform.parent = this.transform;
+
+        //Instantiate(particles, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1.5f), Quaternion.identity);
     }
 }
