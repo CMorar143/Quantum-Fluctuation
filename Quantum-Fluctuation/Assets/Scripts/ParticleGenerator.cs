43177fc617d516d75e648837830edab024fe0f99
CMorar143 <cian@morar.org>
2019-11-27 03:02:19 +0000

    fluctuation occurs every 2 seconds

--- Quantum-Fluctuation/Assets/Scripts/ParticleGenerator.cs
+++ Quantum-Fluctuation/Assets/Scripts/ParticleGenerator.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class ParticleGenerator : MonoBehaviour
 {
     public GameObject particles;
-    private Vector3 playerPos;
 
     void Start()
     {
         //Instantiate(particles, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1.5f), Quaternion.identity);
-        playerPos = transform.position;
-        Instantiate(particles, new Vector3(transform.position.x + Random.Range(-0.4f, 0.4f), Random.Range(-0.3f, 0.2f), transform.position.z), transform.rotation);
+        StartCoroutine("CreateParticles");
+    }
+
+    IEnumerator CreateParticles()
+    {
+        while (true)
+        {
+            Instantiate(particles, new Vector3(transform.position.x + Random.Range(-0.4f, 0.4f), Random.Range(-0.3f, 0.2f), transform.position.z), transform.rotation);
+            yield return new WaitForSeconds(2.0f);
+        }
     }
 }
