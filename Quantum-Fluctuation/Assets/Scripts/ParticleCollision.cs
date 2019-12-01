b03b8cf16734531319e2ca3ba2e75e2c97737df8
CMorar143 <cian@morar.org>
2019-12-01 21:47:13 +0000

    still trying to figure the rotation

--- Quantum-Fluctuation/Assets/Scripts/ParticleCollision.cs
+++ Quantum-Fluctuation/Assets/Scripts/ParticleCollision.cs
@@ -4,18 +4,6 @@ using UnityEngine;
 
 public class ParticleCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "matter")
--- Quantum-Fluctuation/Assets/Scripts/ParticleGenerator.cs
+++ Quantum-Fluctuation/Assets/Scripts/ParticleGenerator.cs
@@ -7,13 +7,13 @@ public class ParticleGenerator : MonoBehaviour
     public GameObject particles;
     private Transform newParticle;
     public float freq = 0.8f;
-    private Quaternion rotUp;
-    private Quaternion rotForward;
-    private Quaternion rotRight;
+    //private Quaternion rotUp;
+    //private Quaternion rotForward;
+    //private Quaternion rotRight;
 
     void Start()
     {
-        //newParticle = this.transform;
+        newParticle = this.transform;
         //Instantiate(particles, new Vector3(transform.position.x, transform.position.y, transform.position.z + 1.5f), Quaternion.identity);
         StartCoroutine("CreateParticles");
     }
@@ -22,25 +22,29 @@ public class ParticleGenerator : MonoBehaviour
     {
         while (true)
         {
-            //transform.rotation = transform.parent.transform.rotation;
-            //Vector3 rot = new Vector3(0, Random.Range(-25.0f, 25.0f), Random.Range(-25.0f, 25.0f));
-            //newParticle.Rotate(rot, Space.Self);
 
-            rotUp = Quaternion.AngleAxis(Random.Range(-25.0f, 25.0f), Vector3.up);
-            rotForward = Quaternion.AngleAxis(Random.Range(-25.0f, 25.0f), Vector3.forward);
-            rotRight = Quaternion.AngleAxis(Random.Range(-25.0f, 25.0f), Vector3.right);
+            Vector3 rot = new Vector3(0, Random.Range(-25.0f, 25.0f), Random.Range(-120f, 120.0f));
+            newParticle.Rotate(rot, Space.World);
+
+            //rotUp = Quaternion.AngleAxis(Random.Range(-25.0f, 25.0f), Vector3.up);
+            //rotForward = Quaternion.AngleAxis(Random.Range(-75.0f, 75.0f), Vector3.forward);
+            //rotRight = Quaternion.AngleAxis(Random.Range(-25.0f, 25.0f), Vector3.right);
+
+            //Quaternion rot = Quaternion.identity * transform.rotation;
+            //rot = rotForward * rot;
+            //rot = rot * 
 
             // Pop into existence
             Instantiate(
-                particles, 
+                particles,
                 new Vector3(
-                    transform.position.x + Random.Range(-0.3f, 0.3f),
+                    transform.position.x,// + Random.Range(-0.3f, 0.3f),
                     0,
                     transform.position.z
                     ),
-                //Quaternion.Euler(new Vector3(0, 0, 0))
-                
+                newParticle.rotation
             );
+            transform.rotation = transform.parent.transform.rotation;
 
             yield return new WaitForSeconds(freq);
         }
