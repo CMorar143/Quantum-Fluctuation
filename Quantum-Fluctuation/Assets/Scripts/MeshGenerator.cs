8495d7a7674ff538b6a020f093fccf748546a902
CMorar143 <cian@morar.org>
2019-12-16 02:42:50 +0000

    added mesh object and script

--- Quantum-Fluctuation/Assets/Scripts/MeshGenerator.cs
+++ Quantum-Fluctuation/Assets/Scripts/MeshGenerator.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour
 {
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        //vertices = 
     }
 
     // Update is called once per frame
