63484a2180fea0c28ad0933a7ae53b155ceb7a4f
CMorar143 <cian@morar.org>
2019-12-16 18:56:18 +0000

    cleaning code

--- Quantum-Fluctuation/Assets/Scripts/MeshGenerator.cs
+++ Quantum-Fluctuation/Assets/Scripts/MeshGenerator.cs
@@ -56,21 +56,6 @@ public class MeshGenerator : MonoBehaviour
             verts++;
         }
 
-        // Just for one quad
-        //vertices = new Vector3[]
-        //{
-        //    new Vector3 (0,0,0),
-        //    new Vector3 (0,0,1),
-        //    new Vector3 (1,0,0),
-        //    new Vector3 (1,0,1)
-        //};
-
-        //triangles = new int[]
-        //{
-        //    0, 1, 2,
-        //    1, 3, 2
-        //};
-
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
@@ -94,16 +79,47 @@ public class MeshGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //this.transform.position = player.position + new Vector3(-xSize/2, -3f, -zSize/2);
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
 
-        // TODO Generate mesh in front
-        // Set the camera in the middle of mesh
-        // Check if the camera is getting close to any edge
-        // Generate in front
-        // Delete what's behind
-        // else
-        // Do nothing and allow the camera to move freely
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
 
+        for (int v = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(z * 0.3f + offsetZ, x * 0.3f + offsetX) * 2f;
+                vertices[v] = new Vector3(x, y, z);
+                v++;
+            }
+        }
 
+        triangles = new int[xSize * zSize * 6];
+
+        for (int tris = 0, verts = 0, z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[0 + tris] = verts + 0;
+                triangles[1 + tris] = verts + xSize + 1;
+                triangles[2 + tris] = verts + 1;
+                triangles[3 + tris] = verts + 1;
+                triangles[4 + tris] = verts + xSize + 1;
+                triangles[5 + tris] = verts + xSize + 2;
+
+                verts++;
+                tris += 6;
+            }
+
+            verts++;
+        }
+
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+        this.transform.position = player.position + new Vector3(-xSize / 2, -3f, -zSize / 2);
     }
 }
