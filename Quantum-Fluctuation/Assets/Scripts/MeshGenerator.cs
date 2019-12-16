941c6e59ce2bae1008fb2cc5ea3eb15960c81f72
CMorar143 <cian@morar.org>
2019-12-16 02:49:02 +0000

    mesh is now quad

--- Quantum-Fluctuation/Assets/Scripts/MeshGenerator.cs
+++ Quantum-Fluctuation/Assets/Scripts/MeshGenerator.cs
@@ -20,17 +20,21 @@ public class MeshGenerator : MonoBehaviour
         {
             new Vector3 (0,0,0),
             new Vector3 (0,0,1),
-            new Vector3 (1,0,0)
+            new Vector3 (1,0,0),
+            new Vector3 (1,0,1)
         };
 
         triangles = new int[]
         {
-            0, 1, 2
+            0, 1, 2,
+            1, 3, 2
         };
 
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
     }
 
     // Update is called once per frame
