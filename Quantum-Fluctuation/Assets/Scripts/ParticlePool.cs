802609ce4dd90d8e3061411207fa1daa7940949b
CMorar143 <cian@morar.org>
2019-12-18 04:55:45 +0000

    created a pool of particles

--- Quantum-Fluctuation/Assets/Scripts/ParticlePool.cs
+++ Quantum-Fluctuation/Assets/Scripts/ParticlePool.cs
@@ -4,23 +4,51 @@ using UnityEngine;
 
 public class ParticlePool : MonoBehaviour
 {
+    static int numParticles = 1000;
+    public GameObject particlePair;
+    static GameObject[] particlePairs;
+
     static int numFluctuations = 80;
     public GameObject fluctuation;
     static GameObject[] fluctuations;
+
 
     // Start is called before the first frame update
     void Start()
     {
         fluctuations = new GameObject[numFluctuations];
-        
         for (int i = 0; i < numFluctuations; i++)
         {
             fluctuations[i] = (GameObject)Instantiate(fluctuation, Vector3.zero, Quaternion.identity);
             fluctuations[i].SetActive(false);
         }
+
+        particlePairs = new GameObject[numParticles];
+        for (int i = 0; i < numParticles; i++)
+        {
+            particlePairs[i] = (GameObject)Instantiate(particlePair, Vector3.zero, Quaternion.identity);
+            particlePairs[i].SetActive(false);
+        }
+
     }
 
-    // Looping through the pool of trees
+    // Looping through the pool of particles
+    // returning any that are inactive
+    static public GameObject GetParticlePairs()
+    {
+        for (int i = 0; i < numParticles; i++)
+        {
+            if (!particlePairs[i].activeSelf)
+            {
+                return particlePairs[i];
+            }
+        }
+
+        // All particles in the pool are being used
+        return null;
+    }
+
+    // Looping through the pool of fluctuations
     // returning any that are inactive
     static public GameObject GetFluctuation()
     {
